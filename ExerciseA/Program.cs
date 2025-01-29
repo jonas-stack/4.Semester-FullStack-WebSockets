a836df5ef73cbc90ff20b9f4d8218f5ddbdd3128
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2025-01-29 12:06:06 +0100

    ExerciseA and ExerciseB week5 2025

--- ExerciseA/Program.cs
+++ ExerciseA/Program.cs
@@ -1,44 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using Fleck;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var server = new WebSocketServer("ws://0.0.0.0:8181");
+server.Start(socket =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    socket.OnOpen = () => Console.WriteLine("Open!");
+    socket.OnClose = () => Console.WriteLine("Close!");
+    socket.OnMessage = message => socket.Send(message);
+});
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+Console.ReadLine();
